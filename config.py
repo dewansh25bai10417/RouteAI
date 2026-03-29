36f40d50f066a893f93223efa3f4c9a13d9524b4
dewansh25bai10417 <dewansh.25bai10417@vitbhopal.ac.in>
2026-03-29 22:45:53 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Warehouse Configuration Settings
+GRID_SIZE = 10
+OBSTACLES = [(1,1), (1,2), (1,3), (3,5), (4,5), (5,5), (7,2), (8,2), (2,8), (3,8), (4,8)]
+START_POINT = (0, 0)
+GOAL_POINT = (9, 9)
+
+# Heuristic Constants
+MANHATTAN_WEIGHT = 1.0
