0040ca7a23ed8a8138da96f07993b7e1bbf62a39
dewansh25bai10417 <dewansh.25bai10417@vitbhopal.ac.in>
2026-03-30 01:24:42 +0530

    Delete src directory

--- src/config.py
+++
@@ -1,8 +0,0 @@
-# Warehouse Configuration Settings
-GRID_SIZE = 10
-OBSTACLES = [(1,1), (1,2), (1,3), (3,5), (4,5), (5,5), (7,2), (8,2), (2,8), (3,8), (4,8)]
-START_POINT = (0, 0)
-GOAL_POINT = (9, 9)
-
-# Heuristic Constants
-MANHATTAN_WEIGHT = 1.0
